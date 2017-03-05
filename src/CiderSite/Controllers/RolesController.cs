cbf939969dbde1eed526a6ed4cb33600a4bda0e1
Steve Burton <stevevburton@gmail.comgit config --global user.email stevevburton@gmail.comgit config --global user.email stevevburton@gmail.com>
2017-03-05 14:50:12 -0800

    add account pages

--- src/CiderSite/Controllers/RolesController.cs
+++ src/CiderSite/Controllers/RolesController.cs
@@ -3,17 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+using CiderSite.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace CiderSite.Controllers
 {
     public class RolesController : Controller
     {
-        // GET: /<controller>/
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public RolesController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<IdentityRoles> roles = _db.Roles.ToList();
+            return View(roles);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string roleName)
+        {
+            try
+            {
+                _db.Roles.Add(new IdentityRole()
+                {
+                    Name = roleName,
+                    NormalizedName = roleName.ToUpper()
+                });
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Roles");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public IActionResult Assign()
+        {
+            List<IdentityRole> roles = _db.Roles.ToList();
+            return View(roles);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Assign(string userName, string userRole)
+        {
+            ApplicationUser user = _db.Users.FirstOrDefault(u => u.UserName == userName);
+            var userResult = await _userManager.AddToRoleAsync(user, userRole);
+
+            return RedirectToAction("Index");
         }
     }
 }
--- src/CiderSite/ViewModels/LoginViewModel.cs
+++ src/CiderSite/ViewModels/LoginViewModel.cs
@@ -7,5 +7,7 @@ namespace CiderSite.ViewModels
 {
     public class LoginViewModel
     {
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
