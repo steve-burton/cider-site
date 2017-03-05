af3f25bcbe48cce0dd756295f5f4ca5e9dd3a59b
Steve Burton <stevevburton@gmail.comgit config --global user.email stevevburton@gmail.comgit config --global user.email stevevburton@gmail.com>
2017-03-05 11:59:29 -0800

    add applicationdbcontext.cs and appsettings.json files

--- src/CiderSite/Models/ApplicationDbContext.cs
+++ src/CiderSite/Models/ApplicationDbContext.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace CiderSite.Models
 {
     public class ApplicationDbContext
     {
+        public DbSet<PublicUser> PublicUsers { get; set; }
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
