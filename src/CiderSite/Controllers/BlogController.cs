646646e3ddaac5664fe70aa5d796da7bd4dcf058
Steve Burton <stevevburton@gmail.com>
2017-03-06 08:17:20 -0800

    add blog model

--- src/CiderSite/Controllers/BlogController.cs
+++ src/CiderSite/Controllers/BlogController.cs
@@ -14,25 +14,25 @@ namespace CiderSite.Controllers
 {
     public class BlogController : Controller
     {
-        private readonly ApplicationDbContext _db;
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly SignInManager<ApplicationUser> _signInManager;
+        //private readonly ApplicationDbContext _db;
+        //private readonly UserManager<ApplicationUser> _userManager;
+        //private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public BlogController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _db = db;
-        }
+        //public BlogController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db)
+        //{
+        //    _userManager = userManager;
+        //    _signInManager = signInManager;
+        //    _db = db;
+        //}
 
-        public BlogController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-        public IActionResult Index()
-        {
-            var blogs = _db.Blogs.ToList();
-            return View(blogs);
-        }
+        //public BlogController(ApplicationDbContext db)
+        //{
+        //    _db = db;
+        //}
+        //public IActionResult Index()
+        //{
+        //    var blogs = _db.Blogs.ToList();
+        //    return View(blogs);
+        //}
     }
 }
---
+++ src/CiderSite/Models/Blog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CiderSite.Models
+{
+    [Table("Blogs")]
+    public class Blog
+    {
+        public Blog()
+        {
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Intro { get; set; }
+        public string BodyCopy { get; set; }
+        public byte[] Data { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        public string getImage()
+        {
+            var base64File = Convert.ToBase64String(Data);
+            return String.Format("data:image/gif;base64,{0}", base64File);
+        }
+    }
+}
